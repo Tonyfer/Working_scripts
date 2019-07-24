aa87d2d7cd99095d17e00a22965be7ae230ffd32
Tonyfer <yt2594@columbia.edu>
2019-07-24 12:39:01 -0400

    dice

--- re_lable/.ipynb_checkpoints/re_label2-checkpoint.py
+++ re_lable/.ipynb_checkpoints/re_label2-checkpoint.py
@@ -6,6 +6,26 @@ import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 import nibabel as nib
 
+
+def dice(im1, im2, empty_score=1.0):
+   
+    im1 = np.asarray(im1).astype(np.bool)
+    im2 = np.asarray(im2).astype(np.bool)
+
+    if im1.shape != im2.shape:
+        raise ValueError("Shape mismatch: im1 and im2 must have the same shape.")
+
+    im_sum = im1.sum() + im2.sum()
+    if im_sum == 0:
+        return empty_score
+
+    # Compute Dice coefficient
+    intersection = np.logical_and(im1, im2)
+
+    return 2*intersection.sum() / im_sum
+
+
+
 def re_label2(ref, target):
     #on_hot encoding
     ref = ref.reshape(-1,1)
