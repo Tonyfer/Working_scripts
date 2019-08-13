daad45a6c90be9e78086cc3b599ee8570a3296b5
Tonyfer <yt2594@columbia.edu>
2019-08-13 09:37:52 -0400

    cut

---
+++ .ipynb_checkpoints/cut_colormap-checkpoint.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import numpy as np
+
+def truncate_colormap(cmapIn='jet', minval=0.0, maxval=1.0, n=100):
+    '''truncate_colormap(cmapIn='jet', minval=0.0, maxval=1.0, n=100)'''    
+    cmapIn = plt.get_cmap(cmapIn)
+
+    new_cmap = colors.LinearSegmentedColormap.from_list(
+        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmapIn.name, a=minval, b=maxval),
+        cmapIn(np.linspace(minval, maxval, n)))
+
+    return new_cmap
+
+cmap_mod = truncate_colormap('Blues',minval=.4, maxval=1)
